4e145f7364295eb40b348df36618f34d2b2e7d69
AyoubElkaoui <elkaoui.a@gmail.com>
2026-02-02 14:33:43 +0100

    fixed issues

--- frontend/components/NotificationBell.tsx
+++ frontend/components/NotificationBell.tsx
@@ -158,25 +158,6 @@ const NotificationBell = () => {
     }
   };
 
-  const getActivityIcon = (activity: Activity) => {
-    try {
-      const activityType = activity.type || "";
-
-      if (activityType === "time_entry") {
-        return (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 text-blue-500"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-              clipRule="evenodd"
-            />
-          </svg>
-        );
   const getNotificationIcon = (type: string) => {
     try {
       if (type === "timesheet_submitted" || type === "timesheet_approved" || type === "timesheet_rejected") {
